f7cda6cef14cb87cb2e583a089d7b47f435db1a3
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-08-15 22:04:13 +0530

    ..

---
+++ LeetCode/Ques205.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        if(n==1) return strs[0];
+        sort(strs.begin(),strs.end());
+        string f=strs[0];
+        string l=strs[n-1];
+        string s="";
+        for(int i=0;i<(min(f.size(),l.size()));i++){
+            if(f[i]==l[i]){
+                s+=f[i];
+            }
+            else return s;
+        }
+        return s;
+    }
+};
